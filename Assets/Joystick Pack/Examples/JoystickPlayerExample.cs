acd3982226a67380695d51d206cf1bda159361a5
DavidTODev <davidto.dev@gmail.com>
2022-10-19 09:03:54 -0500

    [add] made controls 2D

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -7,10 +7,14 @@ public class JoystickPlayerExample : MonoBehaviour
     public float speed;
     public VariableJoystick variableJoystick;
     public Rigidbody rb;
+    public float jumpSpeed = 20;
+    public float runSpeed = 5;
 
     public void FixedUpdate()
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        direction = new Vector3(direction.x, direction.z, direction.y); //Switch Y and Z axis to make game 2D
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+        rb.velocity = new Vector3(20 * direction.x, 100 * direction.y, 0);
     }
 }
