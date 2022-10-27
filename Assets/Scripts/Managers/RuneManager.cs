81a42bbc5f7b906bfbcc96d432a7ada41fe47a72
DavidTODev <davidto.dev@gmail.com>
2022-10-26 20:04:12 -0500

    Improved rune manager

--- Assets/Scripts/Managers/RuneManager.cs
+++ Assets/Scripts/Managers/RuneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RuneManager : MonoBehaviour
 {
@@ -8,9 +9,6 @@ public class RuneManager : MonoBehaviour
     /// <summary>
     /// In charge of all rune and grimoire related
     /// </summary>
-
-    public Canvas playerScreen;
-
     public enum ENUM_RuneType
     {
         ENERGY,
@@ -25,17 +23,17 @@ public class RuneManager : MonoBehaviour
         GOOD_LUCK
     }
 
+    public List<Sprite> collectedRuneSpritesL1;
+
     public List<GameObject> RuneFragmentPrefabsLevel1;
 
-    public enum ENUM_GrimoireType
-    {
-    }
+    public List<Vector2> RuneScreenPositions;
 
     public Canvas PlayerCanvas;
 
     public List<ENUM_RuneType> collectedRunes;
 
-    public List<ENUM_GrimoireType> collectedGrimoires;
+    public float runeSpriteSpacing;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +44,11 @@ public class RuneManager : MonoBehaviour
         //playerScreen = GameObject.Find("Canvas").GetComponent<>;
 
         RuneFragmentPrefabsLevel1 = new List<GameObject>();
+
+        collectedRuneSpritesL1 = new List<Sprite>();
+
+        RuneScreenPositions = new List<Vector2>();
+        RuneScreenPositions.Add(new Vector2(0f - 200f, 0f + 400f));
     }
 
     // Update is called once per frame
@@ -54,9 +57,31 @@ public class RuneManager : MonoBehaviour
         
     }
 
-    public void RuneCaptured(RuneFragmentBehaviour rune)
+    public void RuneCaptured(Sprite runeSprite)
     {
         Debug.Log("RuneManager got rune");
+        collectedRuneSpritesL1.Add(runeSprite);
+        AddRuneToCanvas(runeSprite);
 
+    }
+
+    public void AddRuneToCanvas(Sprite runeSprite)
+    {
+        GameObject imgObject = new GameObject("Rune" + collectedRuneSpritesL1.Count);
+
+        RectTransform trans = imgObject.AddComponent<RectTransform>();
+        trans.transform.SetParent(PlayerCanvas.transform); // setting parent
+        trans.localScale = Vector3.one;
+
+        trans.anchoredPosition = RuneScreenPositions[RuneScreenPositions.Count - 1]; // setting position, will be on center
+        RuneScreenPositions.Add(RuneScreenPositions[RuneScreenPositions.Count - 1] + new Vector2(runeSpriteSpacing, 0f));
+        trans.sizeDelta = new Vector2(150, 200); // custom size
+
+        
+
+        Image image = imgObject.AddComponent<Image>();
+        Texture2D tex = Resources.Load<Texture2D>("red");
+        image.sprite = runeSprite;
+        imgObject.transform.SetParent(PlayerCanvas.transform);
     }
 }
