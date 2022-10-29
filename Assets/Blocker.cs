8b339a559c0a82fd9b89768a1405a71f77005b18
DavidTODev <davidto.dev@gmail.com>
2022-10-28 23:28:20 -0500

    general changes

---
+++ Assets/Blocker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour
+{
+
+    public List<GameObject> list;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
